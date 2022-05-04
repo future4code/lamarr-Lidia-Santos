75239cbd7059ead32302cfb6b1e96f33194ee6bc
unknown <lidia lee abreu dos santos>
2022-05-03 22:34:57 -0300

    lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,18 @@
+
+
+/*let petDeEstimacao= Number(prompt("Quantos pets você tem em casa?"))
+if (petDeEstimacao === 0) {
+    console.log("que pena você pode adotar um pet!")
+} else {
+    for (i = 0 ; i < petDeEstimacao; i++)
+    nomeDoPet = (prompt("diga o nome do seus pets um de cada vez"))
+    arrayDeNomesDePet = []    
+    arrayDeNomesDePet.push(nomeDoPet)
+
+}
+console.log(arrayDeNomesDePet)*/
+
+function arrayOriginal (_listaDeNumeros){
+let _listaDeNumeros = [80, 30, 130, 40, 60, 21,70, 120, 90, 103, 110, 55]
+console.log(_listaDeNumeros)
+}
