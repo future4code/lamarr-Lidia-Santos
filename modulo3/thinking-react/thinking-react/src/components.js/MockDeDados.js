f420eaa90c7a3ae808ac3702d1f9bcb80ba1e52e
unknown <lidia lee abreu dos santos>
2022-07-14 22:47:05 -0300

    exercicio thinking in react

---
+++ modulo3/thinking-react/thinking-react/src/components.js/MockDeDados.js
@@ -0,0 +1,44 @@
+
+const dados=[
+    {
+        id: 1,
+        name: 'Produto 1',
+        price: 123,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+
+    {
+        id: 2,
+        name: 'Produto 2',
+        price: 123,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+
+    {
+        id: 3,
+        name: 'Produto 3',
+        price: 123,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+
+    {
+        id: 4,
+        name: 'Produto 4',
+        price: 123,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+
+    {
+        id: 5,
+        name: 'Produto 5',
+        price: 123,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+
+    {
+        id: 6,
+        name: 'Produto 6',
+        price: 123,
+        photo: 'https://picsum.photos/200/200?a=1',
+    },
+]
